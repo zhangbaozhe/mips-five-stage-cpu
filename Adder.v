29a8f7dce66563f9d65eb1693108a1bf4e54ba5a
Zhang Baozhe <907711755@qq.com>
2021-04-27 15:16:55 +0800

    Adder done!

---
+++ Adder.v
@@ -0,0 +1,26 @@
+/* 
+ * File Name: Adder.v
+ * Author:    ZHANG Baozhe
+ * Date:      April , 2021
+ * ----------------------------
+ * This file implements the adder
+ * in the CPU
+ * 
+ */
+
+module Adder (
+    input [31:0] data1, 
+    input [31:0] data2, 
+    output reg [31:0] result
+);
+
+/* Inputs declaration */
+wire signed [31:0] data1;
+wire signed [31:0] data2;
+
+/* Main function */
+always @(*) begin
+    result = data1 + data2;
+end
+
+endmodule
